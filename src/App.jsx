9dbba3bd33e6c52a4d905f3c95a266663404dcc1
ehirvi <eino.hirvi@gmail.com>
2024-02-08 11:38:12 +0200

    added HTTP request to connect front end to Spring Boot

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import TrashcanService from './services/TrashcanService'
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [trashcans, setTrashcans] = useState([])
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	useEffect(() => getAllTrashcans(), [])
+
+	const getAllTrashcans = () => {
+		TrashcanService
+			.getAll()
+			.then(response => setTrashcans(response))
+	}
+
+	return (
+		<>
+			<ul>
+				{trashcans.map(trashcan =>
+					<li key={trashcan.id}>{trashcan.lon} {trashcan.lat}</li>)}
+			</ul>
+		</>
+	)
 }
 
 export default App
---
+++ src/services/TrashcanService.jsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8080/api/trashcans"
+
+const getAll = () => (
+    axios
+        .get(API_URL)
+        .then(promise => promise.data)
+)
+
+export default { getAll }
