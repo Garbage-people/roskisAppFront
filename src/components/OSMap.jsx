2000a14f2c0fcb141f9e5cad2ef042be5d6cf646
pr0fix <bgx152@myy.haaga-helia.fi>
2024-02-26 19:16:17 +0200

    Changed map default zoom

--- src/components/OSMap.jsx
+++ src/components/OSMap.jsx
@@ -5,13 +5,13 @@ import { Icon } from "leaflet";
 export default function OSMap({ userLat, userLon, trashcans }) {
     const trashIcon = new Icon({
         iconUrl: 'https://cdn-icons-png.flaticon.com/512/1843/1843344.png',
-        iconSize: [32,32],
-        iconAnchor: [16,32],
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
         popupAnchor: [0, -32]
     });
 
     return (
-        <MapContainer center={[userLat, userLon]} zoom={5}>
+        <MapContainer center={[userLat, userLon]} zoom={20}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
