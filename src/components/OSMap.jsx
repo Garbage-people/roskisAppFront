ed4258b9386b49d957ba39a91826a17c854a01e1
pr0fix <bgx152@myy.haaga-helia.fi>
2024-03-11 10:59:54 +0200

    Functionality to update trashcan status

--- src/components/OSMap.jsx
+++ src/components/OSMap.jsx
@@ -1,14 +1,22 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import '../App.css';
 import { Icon } from "leaflet";
+import { useState } from "react";
 
 export default function OSMap({ userLat, userLon, trashcans }) {
+    const [trashCanState, setTrashCanState] = useState({});
+
     const trashIcon = new Icon({
-        iconUrl: 'https://cdn-icons-png.flaticon.com/512/1843/1843344.png',
-        iconSize: [32, 32],
+        iconUrl: 'src//resources//trashbinSlimFGreenGreyEmpty128.png',
+        iconSize: [40, 40],
         iconAnchor: [16, 32],
-        popupAnchor: [0, -32]
+        popupAnchor: [4, -32]
     });
+
+    const handleButtonClick = (id, status) => {
+        const date = new Date().toISOString();
+        setTrashCanState({ id, status, date });
+    };
 
     return (
         <MapContainer center={[userLat, userLon]} zoom={17} minZoom={17} dragging={true}>
@@ -18,12 +26,19 @@ export default function OSMap({ userLat, userLon, trashcans }) {
             />
 
             {trashcans.map(trashcan =>
-                <Marker key={trashcan.id} position={[trashcan.lat, trashcan.lon]} icon={trashIcon}> <Popup>
-                    lat: {trashcan.lat}, lon: {trashcan.lon}
-                </Popup>
+                <Marker key={trashcan.id} position={[trashcan.lat, trashcan.lon]} icon={trashIcon}>
+                    <Popup>
+                        lat: {trashcan.lat}, lon: {trashcan.lon}
+                        <div style={{ display: "flex", justifyContent: "center", margin: "0", padding: "0" }}>
+                            <button id="empty" onClick={() => handleButtonClick(trashcan.id, 0)}><img src="src//resources//trashbinSlimFGreenGreyEmpty128.png" alt="Trashbin" width="40px" height="40px "></img></button>
+                            <button id="full" onClick={() => handleButtonClick(trashcan.id, 1)}><img src="src//resources//trashbinSlimMunsellPinkFull128.png" alt="Trashbin" width="40px" height="40px"></img></button>
+                            <button id="broken" onClick={() => handleButtonClick(trashcan.id, 2)}><img src="src//resources//trashbinSlimBlackGreyOOS128.png" alt="Trashbin" width="40px" height="40px"></img></button>
+                        </div>
+                    </Popup>
                 </Marker>)
             }
-            <Marker position={[userLat, userLon]}/>
-        </MapContainer>
+            <Marker position={[userLat, userLon]} />
+        </MapContainer >
+
     );
 }
