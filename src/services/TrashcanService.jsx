e67015e5c4bf24d2632d8ce8b2750f02c24ef419
Minea <117983209+mineaaa@users.noreply.github.com>
2024-03-20 11:42:03 +0200

    changed to https format

--- src/services/TrashcanService.jsx
+++ src/services/TrashcanService.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const API_URL = "http://roskisappback-backend-garbagepeople.rahtiapp.fi/api/trashcans"
+const API_URL = "https://roskisappback-backend-garbagepeople.rahtiapp.fi/api/trashcans"
 
 const getAll = () => (
     axios
         .get(API_URL)
         .then(promise => promise.data)
-        
+
 )
 
 export default { getAll }
