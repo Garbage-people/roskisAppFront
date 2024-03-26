dd8dcf26a78bd202c1108b508706d410422a6877
pr0fix <bgx152@myy.haaga-helia.fi>
2024-03-26 11:31:46 +0200

    Updated ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,62 +1,62 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    env: {
+        browser: true,
+        es2021: true,
     },
-    "extends": [
+    extends: [
         "eslint:recommended",
-        "plugin:react/recommended"
+        "plugin:react/recommended",
     ],
-    "overrides": [
+    overrides: [
         {
-            "env": {
-                "node": true
+            env: {
+                node: true,
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
+            files: [
+                ".eslintrc.{js,cjs}",
             ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            parserOptions: {
+                sourceType: "script",
+            },
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    "plugins": [
-        "react"
+    plugins: [
+        "react",
     ],
-    "settings":{
-        "react":{
-            "version":"detect"
-        }
+    settings: {
+        react: {
+            version: "detect",
+        },
     },
-    "rules": {
-        "indent": [
+    rules: {
+        indent: [
             "error",
-            4
+            4,
         ],
         "linebreak-style": [
             "error",
-            "unix"
+            "unix",
         ],
-        "quotes": [
+        quotes: [
             "error",
-            "double"
+            "double",
         ],
-        "semi": [
+        semi: [
             "error",
-            "always"
+            "always",
         ],
-        "eqeqeq":"error",
+        eqeqeq: "error",
         "no-trailing-spaces": "error",
         "object-curly-spacing": [
-            "error", "always"
+            "error", "always",
         ],
         "arrow-spacing": [
-            "error", { "before": true, "after": true }
+            "error", { before: true, after: true },
         ],
-        "no-console": 0,
-    }
+        "no-console": "off",
+    },
 };
--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
       setTrashcans(res);
     } catch (err) {
       console.error("Error fetching trashcans", err);
-    }
+    };
   };
 
   const getLocation = () => {
