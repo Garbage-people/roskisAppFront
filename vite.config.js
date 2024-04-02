9379538a8314cfba945c5fcbea77a68443416ce0
ehirvi <eino.hirvi@gmail.com>
2024-04-02 10:34:02 +0300

    Fixed Dockerfile, nginx config and bug that was causing deployment issues

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/roskisAppFront/",
+    // base: "/roskisAppFront/", THIS LINE BREAKS THE NPM BUILD PROCESS
     plugins: [react()],
 });
