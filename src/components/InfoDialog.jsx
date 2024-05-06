ec96ee6ab93681a44bb259e405ce6cf4234e39d7
Aaron Hirsimäki <hirsimakiaaronn@gmail.com>
2024-05-06 11:06:05 +0300

    inffo ikkunan päivitysstä

--- src/components/InfoDialog.jsx
+++ src/components/InfoDialog.jsx
@@ -37,13 +37,22 @@ const InfoDialog = () => {
             height="100px "
           ></img>
         </button>
+
+        <h1>Lähiroskikset</h1>
+        <p>
+        Roskisapp helpottaa Helsingin kaupungissa liikkuvien ja siisteydestä välittävien päivittäistä toimintaa.
+
+        Sovelluksessa on mahdollista päivittää täyttöastetta kuvaava ikoni, jotta sovelluksesta löytyy ajankohtaista tietoa roskisten tilasta. Voit myös lisätä kartalle uuden ikonin, jos huomaat ulkona roska-astian, joka puuttuu kartalta.
+        </p>
+
+        <h2>Kuvakkeet</h2>
         <img
           src="images/RoskisVihreä.png"
           alt="Trashbin"
           width="100px"
           height="100px "
         ></img>
-        <p>Tämä kuvake tarkoittaa roskiksen olevan käytössä</p>
+        <p>Tämä kuvake tarkoittaa roskiksen olevan käyttökelpoinen, eli se ei ole liian täynnä tai rikki.</p>
 
         <img
           src="images/RoskisPunainen.png"
@@ -51,7 +60,7 @@ const InfoDialog = () => {
           width="100px"
           height="100px "
         ></img>
-        <p>Tämä kuvake tarkoittaa roskiksen olevan täynnä</p>
+        <p>Tämä kuvake tarkoittaa roskiksen olevan täynnä.</p>
 
         <img
           src="images/RoskisRuksi.png"
@@ -59,7 +68,20 @@ const InfoDialog = () => {
           width="100px"
           height="100px "
         ></img>
-        <p>Tämä kuva tarkoittaa rikkinäistä roskista</p>
+        <p>Tämä kuva tarkoittaa rikkinäistä, tai jollain tavalla estettyä roskista.</p>
+
+        <img
+          src="images/RoskisLisäysUusi.png"
+          alt="Trashbin"
+          width="100px"
+          height="100px "
+        ></img>
+        <p> 
+            Tällä napilla voit lisätä kartalle uuden roskiskuvakkeen kuvaamaan löytämäsi roskiksen sijaintia. 
+            Lisääminen onnistuu napista, jonka kuvakkeessa roska-astian yllä on + -merkki. 
+            Roska-astian lisääminen onnistuu siinä kohdassa, jossa olet. Et voi valita lisäämiselle paikkaa. 
+            Voit lisätä uuden ikonin vain jos kyseisestä kohdasta ei löydy vielä roska-astian ikonia.
+        </p>
       </dialog>
     </>
   )
